fd84087d70e41569fb84b077baf7dbb86ef1811a
RobnettSean22 <robnettsean22@gmail.com>
2019-09-20 20:15:51 -0700

    3rd commit

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+let evenNumbers = mixedNumbers.filter(function(nums){
+  return nums % 2 === 0
+  
+})
 
 
 
@@ -44,7 +47,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(tax){
+  
+})
 
 
 
@@ -63,7 +68,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(total, sum){
+ return total + sum
+
+})
 
 
 
@@ -89,7 +97,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.map(function(monster){
+  
+})
 
 
 
